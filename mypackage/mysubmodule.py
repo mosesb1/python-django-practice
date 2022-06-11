bab41df2b11db1c1bd68e3969be44dd94df52a2b
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-11 07:31:35 -0400

    added imports

---
+++ mypackage/mysubmodule.py
@@ -0,0 +1,2 @@
+def my_sub_func():
+    print('using a function from mysubmodule.py')
---
+++ myprogram.py
@@ -0,0 +1,8 @@
+from mymodule import useful_func, UsefulClass
+from mypackage.mysubmodule import my_sub_func
+
+myInstance = UsefulClass('hello')
+
+myInstance.report()
+my_sub_func()
+
