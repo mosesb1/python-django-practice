7c05fc91fdfce5d5dbf7be8b569eadb8e2a1522f
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-11 07:24:12 -0400

    dunder methods

--- oop.py
+++ oop.py
@@ -63,7 +63,19 @@ class Agent(Person):
         else:
             self.report()
 
-x = Agent("John","Smith","Viper")
-x.hello()
-x.reveal(121)
-print(x.code_name)
+
+class Book():
+
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __str__(self):
+        return f"{self.title} written by {self.author}"
+    
+    def __len__(self):
+        return self.pages
+    
+mybook = Book('Python rocks', 'jose', 120)
+print(len(mybook))
