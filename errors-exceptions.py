1b12d5e71a341f469ccfc01444947760f632eae1
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-11 06:11:24 -0400

    first commit

---
+++ errors-exceptions.py
@@ -0,0 +1,14 @@
+# try except finally
+
+try:
+    print('10' + 10)
+except IOError:
+    print('You have an input/output error')
+except TypeError:
+    print('You are using the wrong data types')
+except:
+    print('hey you got an error')
+else:
+    print('else block ran')
+finally:
+    print('finally block ran')
