75cc628212c43c1c6e7d20bf9356abc6ed4f0837
Moses Burkhart <moses.burkhart1@gmail.com>
2022-06-11 07:13:01 -0400

    practiced inheritance

--- oop.py
+++ oop.py
@@ -36,6 +36,34 @@ class Circle():
     def multiply_radius(self, num):
         self.radius = self.radius * num
 
-mycircle = Circle(radius = 20)
-mycircle.multiply_radius(2)
-print(mycircle.area(), mycircle.circumference())
+class Person():
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def hello(self):
+        print('hello')
+    
+    def report(self):
+        print(f"I am {self.first_name} {self.last_name}")
+
+class Agent(Person):
+
+    def __init__(self, first_name, last_name, code_name):
+        Person.__init__(self, first_name, last_name)
+        self.code_name = code_name
+
+    def report(self):
+        print('I am here')
+
+    def reveal(self,passcode):
+        if passcode == 123:
+            print('I am a secret agent')
+        else:
+            self.report()
+
+x = Agent("John","Smith","Viper")
+x.hello()
+x.reveal(121)
+print(x.code_name)
